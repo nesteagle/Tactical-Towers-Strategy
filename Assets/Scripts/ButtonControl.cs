74fc205d3facdd3c5e5dbe3b347ef884aacf8223
nesteagle <104323706+nesteagle@users.noreply.github.com>
2024-12-26 21:35:01 -0800

    minor
    removed unused assets

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonControl2 : MonoBehaviour
+public class ButtonControl : MonoBehaviour
 {
     public Spawn Deploy;
 
